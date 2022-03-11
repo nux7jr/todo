44a32d7c209a80e1e6bc70c72c82c957a49ad207
Mike <nux7jr@gmail.com>
2022-03-11 23:00:16 +0700

    1.5

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<template>
+  <div class="container mt-3">
+    <TodoList />
+  </div>
+</template>
+
+<style>
+</style>
